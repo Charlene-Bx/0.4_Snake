b73e09d346f94c0150f0066d5aac33a86791ef96
Charlene-Bx <charlene.bonnardeaux@gmail.com>
2020-11-26 00:39:41 +0100

    Base Javascript

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ Réalisation d'un min jeu Snake avec **JavaScript**.
 ## DOCUMENTATION
 GameDev - [Tutoriel Snake](https://devenirdeveloppeur.wordpress.com/2017/11/15/gamedev-tetris-en-javascript/)
 The Coding Train -[Tutoriel Snake(avec P5.js)](https://www.youtube.com/watch?v=AaGK-fj-BAM&t=186s)
-W3CSchool - [Information Canvas](https://www.w3schools.com/html/html5_canvas.asp)
+W3CSchool - [Information Canvas](https://www.w3schools.com/html/html5_canvas.asp) 
+Mozilla MDN - [Canvas Formes geometriques](https://developer.mozilla.org/fr/docs/Web/API/Canvas_API/Tutoriel_canvas/Formes_g%C3%A9om%C3%A9triques)
 Event KeyCode - [Information Code clavier](https://keycode.info/)
 ### FOR
 Workshop
--- asset/index.js
+++ asset/index.js
@@ -1,96 +1,198 @@
-//0. Attendre que la page soit entiérement chargée avant de lancer le script
-window.onload= () => {  
+//1. Récuperer l'element <canvas> du DOM
+let canvas = document.querySelector("#gameZone"); //ou document.getElementById("gameZone")
 
-    //1. Récuperer l'element <canvas> du DOM
-    let canvas = document.querySelector("#gameZone"); //ou document.getElementById("gameZone")
+//2. Appeler le contexte(=les axes utilisés pour dessiner) de l'element <canvas>
+let contexte= canvas.getContext('2d');
 
-    //2. Appeler le contexte(=les axes utilisés pour dessiner) de l'element <canvas>
-    let contexte= canvas.getContext('2d');
+// Declarer une variable où seront stockee les touches précedement pressée
+let preTouche;
 
-    //3. Initialiser un setInterval qui appelera toutes les fonctions utiles au jeu chaque 10ms.
-    let RunTheGame = setInterval(game,10);  
+// Compter le nombres de pommes mangées
+let pommesMangées=0;
 
-    //4. Définir l'objet serpent
-    const snake={
-        dimension:{
-            largeur:20,
-            hauteur:20
-        },
-        position:{
-            axeX:300,
-            axeY:300
-        },
-        deplacement:{
-            axeX:0,
-            axeY:0
-        },
-        //Voir 5.
-        afficher:()=>{
-            contexte.clearRect(0, 0, canvas.width, canvas.height);
-            contexte.fillStyle="#8db16c";
-            contexte.fillRect(snake.position.axeX, snake.position.axeY, snake.dimension.largeur, snake.dimension.hauteur);
-        },
-        deplacer:()=>{
-            // snake.position.axeX++                =>>Pour le voir se deplacer sur l'axe X
-            snake.position.axeX+=snake.deplacement.axeX;
-            snake.position.axeY+=snake.deplacement.axeY;
+//3. Définir l'objet serpent
+const snake={
+    tete:{
+        largeur:20,
+        hauteur:20
+    },
+    position:{
+        axeX:300,
+        axeY:300
+    },
+    deplacement:{
+        axeX:0,
+        axeY:0
+    },
+    corps:[],
+    taille:3,
+    vitesse:100,
 
+
+    afficher:()=>{
+        contexte.clearRect(0, 0, canvas.width, canvas.height);
+        contexte.fillStyle="#8db16c";
+        // contexte.fillRect(snake.position.axeX, snake.position.axeY, snake.tete.largeur, snake.tete.hauteur);
+        for(let loop=0;loop<snake.corps.length;loop++){
+            contexte.fillRect(snake.corps[loop].x, snake.corps[loop].y, snake.tete.largeur-2, snake.tete.hauteur-2);
         }
-    };
+    },
 
-    //5. Créer la fonction principale du jeu qui sera appeler tous les 10millisecondes
-    function game(){
+    deplacer:()=>{
+        // snake.position.axeX++                =>>Pour le voir se deplacer sur l'axe X
+        snake.position.axeX+=snake.deplacement.axeX*snake.tete.largeur
+        // la position du serpent sur l'axe des X = ((sa position + son deplacement(+1))* sa largeur)
+        snake.position.axeY+=snake.deplacement.axeY*snake.tete.hauteur;
+        let coordonées={};
+        coordonées.x=snake.position.axeX;
+        coordonées.y=snake.position.axeY;
+        snake.corps.push(coordonées);
 
-        //Afficher le serpent
-        snake.afficher();
+        while(snake.corps.length>snake.taille){
+            snake.corps.shift();
+            };
+    },
+    
+    grandir:()=>{  
+        //manger une pomme= position X;Y de la pomme et du serpent = identique
+        console.log("le serpent à manger une pomme!")
+        snake.taille++;
+    },
 
-        //Faire se deplacer le serpent
-        snake.deplacer();
-
-        //Affecter à chaque touche du clavier une modification de la position du serpent
-        document.addEventListener("keydown",clavier);
-        // Pour connaitre les codes de chaque touches utiliser la fonction suivante:
-        // document.addEventListener("keydown",keyboard);
-        // function keyboard(evt){console.log(evt)}
-        // >> keydown {target: body, key: "ArrowUp", charCode: 0, keyCode: 38}
-        function clavier(touche){
-            switch(touche.keyCode) {
-            case 37:
-            // touche gauche
-            snake.deplacement.axeX=-1;              //Se deplace vers la gauche sur l'axe des X
-            snake.deplacement.axeY=0;               //Ne modifie pas l'axe Y
-            break;
-
-            case 38:
-            // touche haut
-            snake.deplacement.axeX=0;              //Ne modifie pas l'axe X
-            snake.deplacement.axeY=-1;             //Se deplace vers le haut de l'axe Y (inversé)
-            break;
-
-            case 39:
-            // touche droite
-            snake.deplacement.axeX=+1;              //Se deplace vers la droite sur l'axe des X
-            snake.deplacement.axeY=0;               //Ne modifie pas l'axe Y
-
-            break;
-            case 40:
-            // touche bas
-            snake.deplacement.axeX=0;              //Ne modifie pas l'axe X
-            snake.deplacement.axeY=+1;             //Se deplace vers le bas de l'axe Y (inversé)
-            break;
-
-            case 32:
-            // touche espace
-            snake.deplacement.axeX=0;              //Ne modifie pas l'axe X
-            snake.deplacement.axeY=0;              //Ne modifie pas l'axe Y
-            break;
-
-            }
-        }
-           
-        
+    accelerer:()=>{
+        snake.vitesse-=10;
     }
 
+
+
+};
+
+const pomme={
+    dimension:{
+        // tete du rayon du cercle = largeur du serpent(20)/2
+        rayon:10,
+    },
+    position:{
+        // Position aléatoire entre 0 et 30 (largeur du canavas(600)/largeur du serpent(20))
+        axeX:(Math.trunc(Math.random()*30)*snake.tete.largeur),
+        axeY:(Math.trunc(Math.random()*30)*snake.tete.hauteur)
+    },
+
+    afficher:()=>{
+        contexte.beginPath();
+        contexte.arc(pomme.position.axeX+pomme.dimension.rayon, pomme.position.axeY+pomme.dimension.rayon, pomme.dimension.rayon, 0, Math.PI * 2);
+        contexte.fillStyle="#df953a";
+        contexte.fill();
+        contexte.closePath();
+    },
+
+    nouvelle:()=>{
+        pomme.position.axeX=(Math.trunc(Math.random()*30)*snake.tete.largeur);
+        pomme.position.axeY=(Math.trunc(Math.random()*30)*snake.tete.hauteur);
+    }
+}
+
+
+window.onload= () => { 
+    // Initialiser un setInterval qui appelera toutes les fonctions utiles au jeu chaque 10ms.
+    let RunTheGame = setInterval(game,snake.vitesse); 
+
+    //Affecter à chaque touche du clavier une modification de la position du serpent
+    document.addEventListener("keydown",clavier);
+}
+
+
+// Créer la fonction principale du jeu qui sera appeler tous les 10millisecondes
+function game(){
+
+    //Afficher le serpent
+    snake.afficher();
+
+    //Faire se deplacer le serpent
+    snake.deplacer();
+
+    //Afficher la pomme
+    pomme.afficher();
+
+    //Verifier si le serpent à manger une pomme
+    if(snake.position.axeX == pomme.position.axeX && snake.position.axeY == pomme.position.axeY){
+        //Faire grandir le serpent
+        snake.grandir();
+
+        //Faire réaparaitre une nouvelle pomme
+        pomme.nouvelle();
+
+        //Compter le nombres de pommes mangées
+        pommesMangées++;
+        
+        //Si nombres de pommes mangées est un multiple de 5, augmentée la vitesse du serpent
+        if(pommesMangées%5===0){
+            snake.accelerer();
+        }
+    }  
+    
+
+
+};
+
+function clavier(touche){
+    // Pour connaitre les codes de chaque touches:
+    // console.log(touche)
+    // >> keydown {target: body, key: "ArrowUp", charCode: 0, keyCode: 38}
+
+    switch(touche.keyCode) {
+    case 37:
+    // touche gauche
+    if(preTouche == 39){break;}             //Si la touche précedente etait la 39, n'excute rien
+
+    snake.deplacement.axeX=-1;              //Se deplace vers la gauche sur l'axe des X
+    snake.deplacement.axeY=0;               //Ne modifie pas l'axe Y
+
+    preTouche= touche.keyCode;              //Stocke la touche préssée dans la variable "preTouche"
+
+
+    break;
+
+    case 38:
+    // touche haut
+    if(preTouche == 40){break;}             //Si la touche précedente etait la 40, n'excute rien
+
+    snake.deplacement.axeX=0;              //Ne modifie pas l'axe X
+    snake.deplacement.axeY=-1;             //Se deplace vers le haut de l'axe Y (inversé)
+
+    preTouche= touche.keyCode;             //Stocke la touche préssée dans la variable "preTouche"
+
+    break;
+
+    case 39:
+    // touche droite
+    if(preTouche == 37){break;}             //Si la touche précedente etait la 37, n'excute rien
+
+    snake.deplacement.axeX=+1;              //Se deplace vers la droite sur l'axe des X
+    snake.deplacement.axeY=0;               //Ne modifie pas l'axe Y
+
+    preTouche= touche.keyCode;              //Stocke la touche préssée dans la variable "preTouche"
+
+    break;
+
+    case 40:
+    // touche bas
+    if(preTouche == 38){break;}             //Si la touche précedente etait la 38, n'excute rien
+
+    snake.deplacement.axeX=0;              //Ne modifie pas l'axe X
+    snake.deplacement.axeY=+1;             //Se deplace vers le bas de l'axe Y (inversé)
+
+    preTouche= touche.keyCode;             //Stocke la touche préssée dans la variable "preTouche"
+
+    break;
+
+    case 32:
+    // touche espace
+    snake.deplacement.axeX=0;              //Ne modifie pas l'axe X
+    snake.deplacement.axeY=0;              //Ne modifie pas l'axe Y
+    break;
+
+    };
 }
 
 
